e9a1298d2be5113c54d3ad7471920da8e51fb714
Brandon Diaz <77746846+brandondia-z@users.noreply.github.com>
2024-05-09 00:19:03 -0400

    readded renderabc.py

---
+++ blender/renderabc.py
@@ -0,0 +1,34 @@
+import bpy
+import sys
+
+def load_alembic(filepath):
+  bpy.ops.wm.alembic_import(filepath=filepath)
+
+def setup_render_settings():
+  bpy.context.scene.render.image_settings.file_format = 'FFMPEG'
+  bpy.context.scene.render.ffmpeg.format = 'MPEG4'
+  bpy.context.scene.render.ffmpeg.codec = 'H264'
+  bpy.context.scene.render.ffmpeg.constant_rate_factor = 'PERC_LOSSLESS'
+  bpy.context.scene.render.filepath = f'//output/snow-{sys.argv[2]}.mp4'
+
+def render_scene():
+  bpy.ops.render.render(animation=True)
+
+def clear_mesh_objects():
+  for obj in bpy.data.objects:
+    if obj.type == 'MESH':
+        bpy.data.objects.remove(obj, do_unlink=True)
+
+def main():
+  # print(sys.argv)
+  clear_mesh_objects()
+
+  alembic_file = bpy.path.abspath('//data/exported_scene.abc')
+
+  load_alembic(alembic_file)
+  setup_render_settings()
+  render_scene()
+
+if __name__ == "__main__":
+    main()
+
